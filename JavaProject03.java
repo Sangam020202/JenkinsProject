1a11120e4a24d500f4f97ba39a8b3f55ef49abb2
Akshita <sangamakshita22@gmail.com>
2022-12-22 18:42:48 +0530

    Second commit

--- JavaProject03.java
+++ JavaProject03.java
@@ -3,6 +3,7 @@ public class JavaProject03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("******************************");
 		System.out.println("Welcome to third java program");
 		System.out.println("******************************");
 	}
